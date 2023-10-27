edc92b743741017b6ac4721b2fa411a009365c55
kta <kta@servers.com>
2023-10-27 16:37:58 +0700

    new task - remove element

--- 27_Remove_Element.py
+++ 27_Remove_Element.py
@@ -1,9 +1,18 @@
 class Solution:
     def removeElement(self, nums: list, val: int) -> int:
-        # Input: nums = [3, 2, 2, 3], val = 3
-        # Output: 2, nums = [2, 2, _, _]
+        # Input: nums = [0,1,2,2,3,0,4,2], val = 2
+        # Output: 2, nums = [0,1,4,0,3]
+        # [0,1,2,2,0,4,2]
         val_count = 0
         while 3 in nums:
             val_count += 1
             nums.remove(3)
-            nums.append('_')
+        element_number = len(nums)
+        return element_number
+
+    def test(self):
+        pass
+
+
+a = Solution()
+n, v = a.removeElement([3,2,2,3], 3)
