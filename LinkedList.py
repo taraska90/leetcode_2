79501396964eccca82ba8022b3b0ec71fcd9806f
kta <kta@servers.com>
2023-11-07 21:30:02 +0700

    linked list isert to the end.

--- LinkedList.py
+++ LinkedList.py
@@ -14,10 +14,24 @@ class SLinkedList:
             newval = newval.next
 
 
+    def find_last_node(self):
+        newval = self.headvalue
+        while newval:
+            if newval.next:
+                newval = newval.next
+            else:
+                last_node = newval
+                return last_node
+
     def insert_beginning(self, begin_value):
         new_node = Node(begin_value)
         new_node.next = self.headvalue
         self.headvalue =  new_node
+
+    def insert_end(self, end_value, linked_list):
+        new_end_node = Node(end_value)
+        last_node = linked_list.find_last_node()
+        last_node.next = new_end_node
 
 """
 list1 = SLinkedList()
