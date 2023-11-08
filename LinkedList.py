41fd8773927bcd859c7bf7599cf35dacfb0b849c
kta <kta@servers.com>
2023-11-08 16:35:57 +0700

    так и не получилось реализовать удаление элемента в llist. Наверно позже вернусь, чёт совсем не получается

--- LinkedList.py
+++ LinkedList.py
@@ -33,11 +33,29 @@ class SLinkedList:
         last_node = linked_list.find_last_node()
         last_node.next = new_end_node
 
+
+    def insert_middle(self, middle_val, new_val):
+        if middle_val is None:
+            print("mentioned node is absent")
+            return
+        new_node = Node(new_val)
+        new_node.next = middle_val.next
+        middle_val.next = new_node
+
+    def remove_node(self, key_to_remove):
+        head_val = self.headvalue
+        while head_val is not None:
+            if head_val.next.value == key_to_remove:
+                head_val.next = head_val.next.next
+            else:
+                head_val = head_val.next
+
+
 """
 list1 = SLinkedList()
-list1.headvalue = Node(10)
-e2 = Node(20)
-e3 = Node(30)
+list1.headvalue = Node("Mon")
+e2 = Node("Tue")
+e3 = Node("Wed")
 list1.headvalue.next = e2
 e2.next = e3
 list1.insert_beginning(5)
